c8c93816e565ca52babe42086f876081c748b1df
Finkle <daniel.finkle1@gmail.com>
2023-04-06 11:53:49 -0700

    fixed html error

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = "it's a secret"
 toolbar = DebugToolbarExtension(app)
 
